351b11e158ee904514a14e68702026af43b63bdf
appreciated <goebeljohannes@gmx.net>
2017-03-07 18:02:43 +0100

    added basic registration validation

--- src/main/java/com/github/appreciated/quickstart/base/navigation/RegistrationControl.java
+++ src/main/java/com/github/appreciated/quickstart/base/navigation/RegistrationControl.java
@@ -6,9 +6,12 @@ import com.github.appreciated.quickstart.base.registration.FieldPropertySet;
 import com.github.appreciated.quickstart.base.registration.RegistrationResult;
 import com.vaadin.data.Binder;
 import com.vaadin.data.HasValue;
+import com.vaadin.data.ValidationResult;
+import com.vaadin.data.Validator;
 import com.vaadin.ui.TextField;
 import javafx.util.Pair;
 
+import javax.xml.ws.Holder;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,14 +23,14 @@ import java.util.stream.Stream;
 public interface RegistrationControl<T> {
     RegistrationResult checkUserRegistrationValidity(T user);
 
-    default Stream<java.lang.reflect.Field> getAttributes() {
+    default Stream<java.lang.reflect.Field> getAttributes(T t) {
         return Arrays.stream(getUser().getClass().getDeclaredFields()).filter(field -> field.isAnnotationPresent(Field.class));
     }
 
     T getUser();
 
-    public default List<Pair<HasValue, java.lang.reflect.Field>> getFields() {
-        Stream<java.lang.reflect.Field> fields = getAttributes();
+    default List<Pair<HasValue, java.lang.reflect.Field>> getFields() {
+        Stream<java.lang.reflect.Field> fields = getAttributes(getUser());
         return fields.map(field -> {
             TextField textField = new TextField();
             textField.setData(field);
@@ -38,10 +41,27 @@ public interface RegistrationControl<T> {
         }).collect(Collectors.toList());
     }
 
-    public default Binder<T> getBinderForFields(List<Pair<HasValue, java.lang.reflect.Field>> components) {
+    default Binder<T> getBinderForFields(List<Pair<HasValue, java.lang.reflect.Field>> components) {
         Binder<T> binder = new Binder<T>().withPropertySet(new FieldPropertySet<T>((Class<T>) getUser().getClass()));
         binder.setBean(getUser());
-        components.forEach(pairConsumer -> binder.bind(pairConsumer.getKey(),pairConsumer.getValue().getName()));
+
+        components.forEach(pairConsumer -> binder.withValidator((Validator<T>) (t, valueContext) -> RegistrationControl.this.isFieldEmpty(t))
+                .bind(pairConsumer.getKey(), pairConsumer.getValue().getName()));
         return binder;
+    }
+
+    default ValidationResult isFieldEmpty(T context) {
+        Holder<Boolean> holder = new Holder<>(Boolean.TRUE);
+        getAttributes(context).forEach((java.lang.reflect.Field field) -> {
+            try {
+                String s = (String) field.get(context);
+                if (s == null || s.length() == 0) {
+                    holder.value = false;
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        });
+        return holder.value ? ValidationResult.ok() : ValidationResult.error("test");
     }
 }
