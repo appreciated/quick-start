e04d74f9ce1f156768eb739ff84ff144d1e2d5ea
appreciated <goebeljohannes@gmx.net>
2017-03-31 02:15:16 +0200

    0.7.8
    - Improvent for Download functionality

--- src/main/java/com/github/appreciated/quickstart/base/navigation/download/Download.java
+++ src/main/java/com/github/appreciated/quickstart/base/navigation/download/Download.java
@@ -4,6 +4,7 @@ import com.vaadin.server.FileDownloader;
 import com.vaadin.server.StreamResource;
 import com.vaadin.ui.Button;
 
+import javax.xml.ws.Holder;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
@@ -14,62 +15,75 @@ import java.nio.file.Paths;
  * Created by appreciated on 22.12.2016.
  */
 public class Download {
-    private String content;
-    private File file;
-    private byte[] bytes;
     private String filename;
+    private ByteDownloadListener byteListener;
+    private StringDownloadListener stringListener;
+    private FileDownloadListener fileListener;
 
-    public Download(String filename, String content) {
-        this.filename = filename;
-        this.content = content;
+    public interface StringDownloadListener {
+        String onDownload();
     }
 
-    public Download(String filename, byte[] bytes) {
-        this.filename = filename;
-        this.bytes = bytes;
+    public interface FileDownloadListener {
+        File onDownload();
     }
 
-    public Download(File file) {
-        this.file = file;
+    public interface ByteDownloadListener {
+        byte[] onDownload();
+    }
+
+    public Download(String filename, StringDownloadListener listener) {
+        this.filename = filename;
+        this.stringListener = listener;
+    }
+
+    public Download(String filename, ByteDownloadListener listener) {
+        this.filename = filename;
+        byteListener = listener;
+    }
+
+    public Download(FileDownloadListener fileDownloadListener) {
+        fileListener = fileDownloadListener;
     }
 
     public void createDownloadButton(Button button) {
-        if (file != null) {
-            createFileDownloadButton(file, button);
-        } else if (content != null) {
-            createStringDownloadButton(content, filename, button);
-        } else if (bytes != null) {
-            createByteDownloadButton(bytes, filename, button);
+        if (fileListener != null) {
+            createFileDownloadButton(fileListener, button);
+        } else if (stringListener != null) {
+            createStringDownloadButton(stringListener, filename, button);
+        } else if (byteListener != null) {
+            createByteDownloadButton(byteListener, filename, button);
         }
     }
 
-
-    public static FileDownloader createFileDownloadButton(File file, Button button) {
+    public static FileDownloader createFileDownloadButton(FileDownloadListener file, Button button) {
+        Holder<File> fileHolder = new Holder<>();
         final StreamResource stream = new StreamResource(
                 (StreamResource.StreamSource) () -> {
                     try {
-                        return new ByteArrayInputStream(Files.readAllBytes(Paths.get(file.toURI())));
+                        fileHolder.value = file.onDownload();
+                        return new ByteArrayInputStream(Files.readAllBytes(Paths.get(fileHolder.value.toURI())));
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                     return null;
-                }, file.getName());
+                }, fileHolder.value.getName());
         FileDownloader downloader = new FileDownloader(stream);
         downloader.extend(button);
         return downloader;
     }
 
-    public static FileDownloader createStringDownloadButton(String fileContent, String fileName, Button button) {
+    public static FileDownloader createStringDownloadButton(StringDownloadListener listener, String fileName, Button button) {
         final StreamResource stream = new StreamResource(
-                (StreamResource.StreamSource) () -> new ByteArrayInputStream(fileContent.getBytes()), fileName);
+                (StreamResource.StreamSource) () -> new ByteArrayInputStream(listener.onDownload().getBytes()), fileName);
         FileDownloader downloader = new FileDownloader(stream);
         downloader.extend(button);
         return downloader;
     }
 
-    public static FileDownloader createByteDownloadButton(byte[] bytes, String fileName, Button button) {
+    public static FileDownloader createByteDownloadButton(ByteDownloadListener listener, String fileName, Button button) {
         final StreamResource stream = new StreamResource(
-                (StreamResource.StreamSource) () -> new ByteArrayInputStream(bytes), fileName);
+                (StreamResource.StreamSource) () -> new ByteArrayInputStream(listener.onDownload()), fileName);
         FileDownloader downloader = new FileDownloader(stream);
         downloader.extend(button);
         return downloader;
